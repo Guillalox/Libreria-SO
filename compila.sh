dca61901ec9f49c2c003ec649b8b460566f45f7e
Guille <g.romero.2021@alumnos.urjc.es>
2023-11-08 15:32:47 +0100

    Memory leaks

--- compila.sh
+++ compila.sh
@@ -3,7 +3,7 @@
 gcc -c libreria.c
 ar rcs libreria.a libreria.o
 
-gcc -Wall -Wextra -o test test.c -L./ -l:libreria.a
+gcc -Wall -Wextra -o test test.c -fsanitize=address -L./ -l:libreria.a 
 
 rm libreria.o
 
